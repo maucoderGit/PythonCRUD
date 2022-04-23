cf5c26d2333303b45f6c1109b19e35ab39006cba
maucoder <dreg5000.bbb@gmail.com>
2022-04-23 17:22:23 -0400

    my first python program with grafic interfase

---
+++ tortuga.py
@@ -0,0 +1,18 @@
+import turtle
+
+window = turtle.Screen()
+turtle = turtle.Turtle()
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+window.mainloop()
