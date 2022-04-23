f408c09a508f313cf6782d169386d6f5068334af
maucoder <dreg5000.bbb@gmail.com>
2022-04-23 19:50:27 -0400

    Intro proyect - class completed

--- Proyect/main.py
+++ Proyect/main.py
@@ -1,2 +1,10 @@
+CLIENTS = ["Juan", "Jose", "Pedro"]
+
+def run():
+    counter = 0
+    for i in CLIENTS:
+        print(f'{counter}. {CLIENTS[counter]}')
+        counter += 1
+
 if __name__ == '__main__':
-    pass
+    run()
