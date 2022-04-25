0f9be17094161413522c7c37b16a58d4ebc6a887
maucoder <dreg5000.bbb@gmail.com>
2022-04-24 23:46:16 -0400

    adding POO to the proyect, need to work on it

--- Proyect/clients.py
+++ Proyect/clients.py
@@ -3,5 +3,14 @@ class Client:
     Create client's object
     """
     def __init__(self):
-        pass
+        self._name: str
     
+    # returns atribute's name
+    @property
+    def name(self):
+        return self._name
+    
+    # Set value to atribute name
+    @name.setter
+    def name(self, name):
+        self._name = name
